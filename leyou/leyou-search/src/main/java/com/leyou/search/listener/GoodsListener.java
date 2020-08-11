afff7f719c9dc61776c9b1e3ef990b17f5505a8c
yuanqianchen <1151575134@qq.com>
2020-08-11 17:25:46 +0800

    leyou项目：day15，rabbitmq，消息同步

---
+++ leyou/leyou-search/src/main/java/com/leyou/search/listener/GoodsListener.java
@@ -0,0 +1,50 @@
+package com.leyou.search.listener;
+
+import com.leyou.search.service.SearchService;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GoodsListener {
+
+    @Autowired
+    private SearchService searchService;
+
+    /**
+     * 保存或更新
+     * @param id
+     * @throws IOException
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "LEYOU.SEARCH.SAVE.QUEUE", durable = "true"),
+            exchange = @Exchange(value = "LEYOU.ITEM.EXCHANGE", ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC),
+            key = {"item.insert","item.update"}
+    ))
+    public void save(Long id) throws IOException {
+        if(id == null){
+            return ;
+        }
+
+        this.searchService.save(id);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "LEYOU.SEARCH.DELETE.QUEUE", durable = "true"),
+            exchange = @Exchange(value = "LEYOU.ITEM.EXCHANGE", ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC),
+            key = {"item.delete"}
+    ))
+    public void delete(Long id) {
+        if(id == null){
+            return ;
+        }
+
+        this.searchService.delete(id);
+    }
+}
